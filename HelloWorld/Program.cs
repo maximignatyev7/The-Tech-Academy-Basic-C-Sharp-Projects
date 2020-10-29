86b01514149d7cc80446266ab3b4d0162aea4c04
Max <maximignatyev7@gmail.com>
2020-10-29 09:49:23 -0700

    added files
    accounting inside

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+
+        string doubleQuotes = ("The use of double quotes\" \"is necessary!");
+        Console.WriteLine(doubleQuotes);
+        Console.ReadLine();
+
+        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}

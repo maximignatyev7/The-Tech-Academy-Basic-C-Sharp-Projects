8fc4e1d266befe051241c3119f68a29a3785b15e
Max <maximignatyev7@gmail.com>
2020-11-03 16:03:45 -0800

    Added Loop_while_do
    Added Loop_while_do folder

---
+++ Loop_while_do/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_while_do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+            Console.ReadLine();
+
+
+            //int n = 0;
+            //while (n < 5)
+            //{
+            //    Console.WriteLine(n);
+            //    n++;
+            //}
+            //Console.ReadLine();
+
+            //int n = 0;
+            //do
+            //{
+            //    Console.WriteLine(n);
+            //    n++;
+            //}
+            //while (n < 5);
+            //Console.ReadLine();
+        }
+    }
+}

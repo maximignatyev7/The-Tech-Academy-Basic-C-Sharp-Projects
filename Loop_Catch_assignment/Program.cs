7af3a4238d231f3b8868f9779605643a09661285
Max <maximignatyev7@gmail.com>
2020-11-06 10:03:54 -0800

    Added Loop_Catch_assignment
    Added Loop_Catch_assignment folder

---
+++ Loop_Catch_assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Catch_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10, 25, 40, 65, 70 };
+            Console.Write("Please enter whole number up to 10: ");
+            
+
+            try
+            {                
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("We have list of numbers: 10, 25, 40, 65, 70");
+                Console.WriteLine("Divide by " + userNum + ", you will get such results:\n");
+
+                foreach (int number in numbers)
+                {
+
+                    Console.WriteLine(number / userNum);
+                }
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please do not divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

3894774f675567207519821673d9c8d91bcd13c3
Max <maximignatyev7@gmail.com>
2020-11-08 10:23:29 -0800

    Added Method_Class_assignment_2
    Added Method_Class_assignment_2 folder

---
+++ Method_Class_assignment_2/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_assignment_2
+{
+    class MyClass
+    {
+        public int Addition(int x)
+        {
+            return 100 + x;
+        }
+
+        public int Subtraction(int x)
+        {
+            return 100 - x;
+        }
+
+        public int Multiplication(int x)
+        {
+            return 100 * x;
+        }
+    }
+}
---
+++ Method_Class_assignment_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_assignment_2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyClass superNum = new MyClass();
+            
+            Console.Write("Please enter whole number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("100 plus " + userNum + " is " + superNum.Addition(userNum));
+
+            Console.WriteLine("100 minus " + userNum + " is " + superNum.Subtraction(userNum));
+
+            Console.WriteLine("100 multiply by " + userNum + " is " + superNum.Multiplication(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}

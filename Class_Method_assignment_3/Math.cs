c608f298841afc5d2804318db0dc04c50b783178
Max <maximignatyev7@gmail.com>
2020-11-08 14:00:49 -0800

    Added Class_Method_assignment_3
    Added Class_Method_assignment_3 folder

---
+++ Class_Method_assignment_3/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_3
+{
+    class Math
+    {
+        public int Division(int y)
+        {
+            return 80 / y;
+        }
+
+        public double Multiplication(double y)
+        {
+            return 80 * y;
+        }
+
+        public string Addition(string y)
+        {           
+            //int convertString = Convert.ToInt32(Multiplication);
+            return 80 + y;
+        }
+    }
+}
---
+++ Class_Method_assignment_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math divisionObj = new Math();
+            
+
+            Console.WriteLine("80 divided by 2 is " + divisionObj.Division(2));
+            Console.WriteLine("80 multiply by 20.25 is " + divisionObj.Multiplication(20.25));
+
+
+
+            //int z = Convert.ToInt32(divisionObj.Addition("40"));
+
+            Console.WriteLine("80 plus 40 is " + divisionObj.Addition(z));
+
+            Console.ReadLine();
+        }
+    }
+}

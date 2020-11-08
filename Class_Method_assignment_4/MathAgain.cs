5bbb246aa3f6d601b2f4f8f4c149b3aa179329dd
Max <maximignatyev7@gmail.com>
2020-11-08 15:05:37 -0800

    Added Class_Method_assignment_4
    Added Class_Method_assignment_4 folder

---
+++ Class_Method_assignment_4/MathAgain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_4
+{
+    class MathAgain
+    {
+        public int Addition(int x, int y = 100)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Class_Method_assignment_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathAgain mathObj = new MathAgain();
+            Console.Write("Enter first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number:");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            //int sumNumber = mathObj.Addition(firstNum, secondNum);
+            //Console.WriteLine(sumNumber);
+            Console.WriteLine(mathObj.Addition(firstNum, secondNum));
+
+            Console.Write("Also, It is gonna work without second number!\n");
+
+            Console.WriteLine(mathObj.Addition(firstNum));
+
+            Console.ReadLine();
+        }
+    }
+}

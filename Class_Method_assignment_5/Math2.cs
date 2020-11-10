dcf128f658e9a022df7d2bf009285a3d3fcb7b92
Max <maximignatyev7@gmail.com>
2020-11-09 20:26:02 -0800

    Added Class_Method_assignment_5
    Added Class_Method_assignment_5 folder

---
+++ Class_Method_assignment_5/Math2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_5
+{
+    class Math2
+    {
+        public void Operation(int x, int y)
+        {
+            x = x + 100;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+        }
+    }
+}
---
+++ Class_Method_assignment_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math2 mathObj = new Math2();
+
+            int firstNum = 10;
+            int secondNum = 20;
+
+            mathObj.Operation(5, 15);
+
+            //mathObj.Operation(firstNum, secondNum);
+
+            Console.ReadLine();
+        }
+    }
+}
